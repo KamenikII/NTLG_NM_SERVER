588fd908079031771cdc82ae0bbd6451c2bf410e
Ilya Kamenik <kamenikii@yandex.ru>
2023-02-03 10:59:10 +0300

    First step of server, HW2

---
+++ src/main/kotlin/ru/netology/nmedia/NMediaApplication.kt
@@ -0,0 +1,52 @@
+package ru.netology.nmedia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NMediaApplication {
+    @Bean
+    fun runner(service: PostService) = CommandLineRunner {
+        with(service) {
+            save(
+                Post(
+                    id = 0,
+                    author = "Netology",
+                    authorAvatar = "netology.jpg",
+                    content = "Привет, это новая Нетология!",
+                    published = 0,
+                    likedByMe = false,
+                    likes = 0,
+                )
+            )
+            save(
+                Post(
+                    id = 0,
+                    author = "Сбер",
+                    authorAvatar = "sber.jpg",
+                    content = "Привет, это новый Сбер!",
+                    published = 0,
+                    likedByMe = false,
+                    likes = 0,
+                )
+            )
+            save(
+                Post(
+                    id = 0,
+                    author = "Тинькофф",
+                    authorAvatar = "tcs.jpg",
+                    content = "Нам и так норм!",
+                    published = 0,
+                    likedByMe = false,
+                    likes = 0,
+                )
+            )
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<NMediaApplication>(*args)
+}
+
+
---
+++ src/main/kotlin/ru/netology/nmedia/entity/PostEntity.kt
@@ -0,0 +1,24 @@
+package ru.netology.nmedia.entity
+
+import ru.netology.nmedia.dto.Post
+import jakarta.persistence.Id
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+
+@Entity
+data class PostEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long,
+    var author: String,
+    var content: String,
+    var published: Long,
+    var likedByMe: Boolean,
+    var likes: Int = 0,
+    var authorAvatar: String? = null,
+) {
+    fun toDto() = Post(id, author, content, published, likedByMe, likes)
+
+    companion object {
+        fun fromDto(dto: Post) = PostEntity(dto.id, dto.author, dto.content, dto.published, dto.likedByMe, dto.likes)
+    }
+}
